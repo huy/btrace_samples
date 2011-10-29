3a6df31d5a19defffb21a1c9535f0dc74a99bc0b
Le Huy <lehuy20@gmail.com>
2011-10-30 00:04:43 +0200

    some comments

--- Hello.java
+++ Hello.java
@@ -3,12 +3,12 @@ public class Hello {
 	public static void main(String argv[]){
 		System.out.println("Hello World");
 
-		//sleep here, so btrace can attach to the process at this point
 		//before this point, the program is executed by original code
+		//sleep here, so btrace can attach to the process at this point
 		if (argv.length > 0)
 			sleep(Integer.parseInt(argv[0])); 
 		// assuming that btrace has attached and instrumented desired classes here then 
-                // the following line will be executed by instrumented code 
+                // the following line will be executed by code being instrumented
 		System.out.println("Hello Moon"); 
 	}
 
